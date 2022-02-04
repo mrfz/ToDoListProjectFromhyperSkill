7cc8058f6670c7eb72d024238f4c56e1c3a82bec
mrfz <zlobin.f@gmail.com>
2022-02-04 22:52:10 +0300

    Completed all To-Do List project from HyperSkill. Still can improve style, but algorithm is working

--- To-Do List/task/src/assets/taskManager.js
+++ To-Do List/task/src/assets/taskManager.js
@@ -1,18 +1,20 @@
 let addTaskButton = document.getElementById ("add-task-button");
 let taskInput = document.getElementById("input-task");
-let taskList = document.getElementById('task-list')
+let taskHTMLList = document.getElementById('task-list');
+let taskList = [];
+addTaskButton.addEventListener("click",() => createNewTask (taskInput.value, false));
 
-addTaskButton.addEventListener("click", createNewTask);
 
-
-function createNewTask () {
-  if (taskInput.value != "") {
-     let newTask = taskInput.value;
+function createNewTask (taskName, isChecked) {
+  if (taskName) {
+     let newTask = taskName;
      let newLi = document.createElement ('li')
      let newCheckBox = document.createElement('input');
      let newSpan = document.createElement('span');
      let newButton = document.createElement('button');
      newCheckBox.type = "checkbox";
+     newCheckBox.checked = isChecked;
+     newCheckBox.setAttribute('onclick','saveTaskList()')
      newSpan.className = "task";
      newSpan.innerHTML = newTask;
      newButton.className = "delete-btn";
@@ -22,10 +24,29 @@ function createNewTask () {
       newLi.appendChild(newCheckBox);
       newLi.appendChild(newSpan);
       newLi.appendChild(newButton);
-      taskList.appendChild(newLi);
+      taskHTMLList.appendChild(newLi);
+      taskInput.value = "";
+      saveTaskList();
   }
 }
 
 function deleteTask (completedTask) {
     completedTask.parentNode.remove();
+    saveTaskList();
 }
+
+function saveTaskList() {
+    let taskList = []
+    for (let task of document.querySelectorAll("li")) {
+        taskList.push({'taskName':task.querySelector("span").innerText, 'isChecked':task.querySelector("input").checked});
+    }
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+}
+
+function getTasksFromStorage () {
+    let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
+    for (let task of taskList) {
+        createNewTask(task['taskName'], task['isChecked']);
+    }
+}
+
